d0e27f1b3af8e5fe26d7876f25066541ac9cb1c1
mohamed <chakch007@gmail.com>
2018-01-16 19:31:38 +0100

    add missing files

--- app.js
+++ app.js
@@ -3,51 +3,22 @@ var assert      = require('assert');
 var express    	= require('express');
 var app        	= express();
 var bodyParser 	= require('body-parser');
+var User 		= require('./db/user.js');
+var mongoose 	= require('mongoose');
 
-app.use(bodyParser.urlencoded({extend: true}));
-app.use(bodyParser.json());
+var db_tools = require('./tools/db_tools');
 
 var port = 3000;
 
+
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.json());
+var routes = require('./routes/routes');
 var router = express.Router();
-
-
+routes.assignRoutes(router);
+app.use('/api', router);
 app.listen(port);
 console.log('Magic happens on port ' + port);
-// Connection URL
 
-var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost:27017');
-console.log(connection);
-var User = require('./db/user.js');
-console.log(User);
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
+	
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-mongoose.set('debug', true);
-
-router.route('/users')
-	.post(function(req,res){
-		var user = new User();
-		user.name = 'mohamed';
-		user.save(function(err){
-			if(err) res.send(err);
-			res.json({message: 'user created'})
-		})
-	})
-	.get(function(req,res){
-
-		console.log("get on api/users");		
-		user.find({},function(err,users){
-			if(err) res.send(err);
-			console.log(users);		
-			res.json(users);
-		})
-	});
-app.use('/api', router);
--- db/user.js
+++ db/user.js
@@ -1,10 +1,51 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+var db_tools = require('../tools/db_tools');
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
 
 mongoose.set('debug', true);
 
 var UserSchema   = new Schema({
-    name: String
+    name   : String,
+    prenom : String
 });
 
-module.exports = mongoose.model('users',UserSchema);
+var User = mongoose.model('User',UserSchema,"users");
+exports.User = User;
+
+
+exports.findAllUsers = function(){
+	return new Promise(function(resolve,reject){
+		db_tools.DBConnectMongoose();
+		console.info("[USER] find all");
+		User.find(function(err,users){
+			if(err) {
+				console.log(err);
+				reject(err);
+			}
+			console.log(users);
+			resolve(users);
+		})		
+	})
+}
+  var Projection = {
+        __v    	: false,
+        _id    	: false,
+        name 	: true,
+         //you can add many parameters here for projection
+    };
+exports.findUser = function(userName){
+	return new Promise(function(resolve,reject){
+		db_tools.DBConnectMongoose();
+		console.info("[USER] find user" , userName);
+		User.findOne({'name' : userName})
+		.select('name prenom ')
+		.exec(function(err,users){
+			if(err) {
+				console.log(err);
+				reject(err);
+			}
+			console.log(users);
+			resolve(users);
+		})		
+	})
+}
